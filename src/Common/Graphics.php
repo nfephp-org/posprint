c0c5f243ce98aceba163b079b2bc63d0346c6dc9
Roberto L. Machado <linux.rlm@gmail.com>
2015-05-15 15:37:49 -0300

    [UPD] Graphics.php

--- src/Common/Graphics.php
+++ src/Common/Graphics.php
@@ -12,7 +12,12 @@ class Graphics
     public static $imgWidth;
 
     /**
+     * Construtor 
+     * Carrega uma imagem, se for passada e ajusta suas dimensões
      * 
+     * @param string $filename
+     * @param int $width
+     * @param int $height
      */
     public function __construct($filename = '', $width = null, $height = null)
     {
@@ -20,7 +25,8 @@ class Graphics
     }
 
     /**
-     * 
+     * loadImage
+     * Carrega uma imagem, se for passada e ajusta suas dimensões
      * @param type $resource
      * @param type $width
      * @param type $height
@@ -30,7 +36,7 @@ class Graphics
         if (! is_file($filename)) {
             return;
         }
-        if (!is_readable($filename)) {
+        if (! is_readable($filename)) {
             throw new Exception("Não é possivel ler esse arquivo '$filename' Permissões!!");
         }
         $tipo = self::zIdentifyImg($filename);
@@ -49,36 +55,68 @@ class Graphics
     }
     
     /**
-     * 
-     * @return type
+     * getWidth
+     * Retorna a latgura em pixels
+     * @return int
      */
-    public static function getImageRaster()
+    public static function getWidth()
     {
-        return self::zGetBinaryImage(self::$img);
+        return self::$imgWidth;
     }
     
     /**
-     * 
-     * @param type $width
-     * @param type $height
+     * getHeight
+     * Retorna a altura em pixels
+     * @return int
+     */
+    public static function getHeight()
+    {
+        return self::$imgHeight;
+    }
+    
+    /**
+     * getImageBinary
+     * Retorna a representação em Bytes da imagem
+     * @return type
+     */
+    public static function getImageBinary()
+    {
+        return self::zPixel2Byte();
+    }
+    
+    /**
+     * resizeImage
+     * Redimensiona uma imagem
+     * NOTA: a largura será sempre ajustada para o multiplo de 8 mais
+     * proximo, por que as impressoras tem uma resolução de 8 dots
+     * @param int $width
+     * @param int $height
      */
     public static function resizeImage($width = null, $height = null)
     {
+        if ($width == null && $height == null) {
+            return;
+        }
         if ($width != null && $height == null) {
+            $width = self::zAdjustMultiple($width);
             $razao = $width / self::$imgWidth;
-            $height = (int) round($razao * self::$imgHeight, 0);
+            $height = (int) round($razao * self::$imgHeight);
         } elseif ($width == null && $height != null) {
             $razao = $height / self::$imgHeight;
-            $width = (int) round($razao * self::$imgWidth, 0);
+            $width = (int) round($razao * self::$imgWidth);
+            $width = self::zAdjustMultiple($width);
+        } elseif ($width != null && $height != null) {
+            $width = self::zAdjustMultiple($width);
         }
         $tempimg = imagecreatetruecolor($width, $height);
         imagecopyresampled($tempimg, self::$img, 0, 0, 0, 0, $width, $height, self::$imgWidth, self::$imgHeight);
         self::$img = $tempimg;
         self::zLoadDimImage();
     }
-
+    
     /**
-     * 
+     * getImageQRCode
+     * Gera uma imagem GD do QRCode 
      * @param int $size
      * @param type $padding
      * @param type $errCorretion LOW, MEDIUM, QUARTILE, HIGH
@@ -94,6 +132,7 @@ class Graphics
         if ($dataText == '') {
             return;
         }
+        $width = self::zAdjustMultiple($width);
         $qrCode = new QrCode();
         $qrCode->setText($dataText)
                ->setImageType('png')
@@ -107,34 +146,86 @@ class Graphics
         self::$img = $qrCode->getImage();
         self::zLoadDimImage();
     }
-
-    public static function getImage()
+    
+    /**
+     * getGDobj
+     * Retorna um objeto GD
+     * @return GD
+     */
+    public static function getGDobj()
     {
         return self::$img;
     }
     
     /**
-     * 
-     * @return type
+     * zPixel2Byte
+     * Converte cada pixel da imagem em um byte,
+     * cada byte irão representar 8 dots 
+     * @return array
+     * @throws Exception
      */
-    protected static function zGetBinaryImage()
+    protected static function zPixel2Byte()
     {
-        $data = "";
-        for ($yPos = 0; $yPos < self::$imgHeight; $yPos++) {
-            for ($xPos = 0; $xPos < self::$imgWidth; $xPos++) {
-                $rgb = imagecolorat(self::$img, $xPos, $yPos);
-                $red = ($rgb >> 16) & 0xFF;
-                $green = ($rgb >> 8) & 0xFF;
-                $blue = $rgb & 0xFF;
-                $gray = (int)(($red + $green + $blue) / 3);
-                $data .= chr($gray);
+        $widthPixels = self::$imgWidth;
+        $heightPixels = self::$imgHeight;
+        //numero de bytes da imagem
+        $widthBytes = (int)((self::$imgWidth + 7) / 8);
+        $imgData = self::zPixel2BitBW(self::$img, $widthPixels, $heightPixels);
+        $xPos = $yPos = $bit = $byte = $byteVal = 0;
+        $data = str_repeat("\0", $widthBytes * $heightPixels);
+        do {
+            $byteVal |= (int) $imgData[$yPos * $widthPixels + $xPos] << (7 - $bit);
+            $xPos++;
+            $bit++;
+            if ($xPos >= $widthPixels) {
+                $xPos = 0;
+                $yPos++;
+                $bit = 8;
+                if ($yPos >= $heightPixels) {
+                    $data[$byte] = chr($byteVal);
+                    break;
+                }
             }
-        }
+            if ($bit >= 8) {
+                $data[$byte] = chr($byteVal);
+                $byteVal = 0;
+                $bit = 0;
+                $byte++;
+            }
+        } while (true);
         return $data;
     }
     
     /**
-     * 
+     * zPixel2BitBW
+     * Converte a imagem em uma representação de seus bits com a 
+     * imagem convertida para BW
+     * @param GD $img
+     * @param int $widthPixels
+     * @param int $heightPixels
+     * @return array
+     */
+    private static function zPixel2BitBW($img, $widthPixels, $heightPixels)
+    {
+        //cria uma matriz com zeros hex que representa uma imagem em branco
+        $imgData = str_repeat("\0", $heightPixels * $widthPixels);
+        for ($yPos = 0; $yPos < $heightPixels; $yPos++) {
+            for ($xPos = 0; $xPos < $widthPixels; $xPos++) {
+                //pega a cor do pixel da imagem e converte em cinza
+                $colors = imagecolorsforindex($img, imagecolorat($img, $xPos, $yPos));
+                $greyness = (int)($colors['red'] + $colors['red'] + $colors['blue']) / 3;
+                $black = (255 - $greyness) >> (7 + ($colors['alpha'] >> 6));
+                //carrega a matriz com o tom de cinza
+                $imgData[$yPos * $widthPixels + $xPos] = $black;
+            }
+        }
+        return $imgData;
+    }
+
+    /**
+     * zLoadDimImage
+     * Obtêm a largura e a altura da imagem em pixels e 
+     * colo nas propriedades da classe
      */
     private static function zLoadDimImage()
     {
@@ -144,6 +235,7 @@ class Graphics
     
     /**
      * zIdentifyImg
+     * Identifica o tipo de imagem
      * @param string $filename
      * @return string
      */
@@ -167,5 +259,21 @@ class Graphics
                 $typo = 'none';
         }
         return $typo;
+    }
+    
+    /**
+     * zAdjustMultiple
+     * Ajusta o numero para o multipo mais proximo de 8
+     * @param int $num
+     * @return int
+     */
+    private static function zAdjustMultiple($num = 0, $base = 8)
+    {
+        //caso a largura da imagem em pixels não seja multiplo de $base
+        //converter no multiplo de 8 mais proximo
+        if ($num % $base != 0) {
+            $num = round($num/$base) * $base;
+        }
+        return $num;
     }
 }
--- tests/Common/GraphicsTest.php
+++ tests/Common/GraphicsTest.php
@@ -3,29 +3,68 @@
  * Class Graphics
  * @author Roberto L. Machado <linux.rlm at gmail dot com>
  */
-namespace Posprint\tests\Common;
 
 use Posprint\Common\Graphics;
 
 class GraphicsTest extends PHPUnit_Framework_TestCase
 {
-    public function testcreateQRCodeImg()
+    protected $folderBase;
+    protected $graf;
+    
+    protected function setUp()
     {
-        
+        $this->folderBase = dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR.'fixtures'.DIRECTORY_SEPARATOR;
+        $this->graf = new Graphics();
+    }
+    
+    public function testgetImageQRCode()
+    {
+        $msg = 'https://www.sefaz.rs.gov.br/NFCE/NFCE-COM.aspx?chNFe=43141006354976000149650540000086781171025455&nVersao=100&tpAmb=2&dhEmi=323031342d31302d33305431353a33303a32302d30323a3030&vNF=0.10&vICMS=0.00&digVal=682f4d6b6b366134416d6f7434346d335a386947354f354b6e50453d&cIdToken=000001&cHashQRCode=771A7CE8C50D01101BDB325611F582B67FFF36D0';
+        $this->graf->getImageQRCode(200, 1, 'low', $msg);
+        $raster = $this->graf->getImageBinary();
+        $fixture = file_get_contents($this->folderBase.'qrdemo.dat');
+        $this->assertEquals($raster, $fixture);
     }
     
     public function testLoadImg()
     {
-        
+        $filename = $this->folderBase.'tux.jpg';
+        $this->graf->loadImage($filename);
+        $wdt = $this->graf->getWidth();
+        $this->assertEquals($wdt, 196);
+    }
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage 
+     */
+    public function testFailLoadImg()
+    {
+        $filename = $this->folderBase.'tux.bmp';
+        $this->graf->loadImage($filename);
     }
     
-    public function testGetRaster()
+    
+    public function testGetImageBinary()
     {
-        
+        $filename = $this->folderBase.'tux.jpg';
+        $this->graf->loadImage($filename, 208);
+        $wdt = $this->graf->getWidth();
+        $this->assertEquals($wdt, 208);
     }
     
     public function testResizeImage()
     {
-        
+        $filename = $this->folderBase.'tux.jpg';
+        $this->graf->loadImage($filename, 208, 229);
+        $wdt = $this->graf->getWidth();
+        $hgt = $this->graf->getHeight();
+        $this->assertEquals($wdt, 208);
+        $this->assertEquals($hgt, 229);
+    }
+    
+    public function testGetImage()
+    {
+        $this->assertTrue(true);
     }
 }
