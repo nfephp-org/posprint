460a6bfc999f30b478ac90b40645bbf0e8c85d6c
Roberto L. Machado <linux.rlm@gmail.com>
2016-03-11 17:26:53 -0300

    [UPD] terceira limpeza

--- src/Emulator/Emulator.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/* 
- * A ideia por traz desse emulador é criar uma imagem da impressão real usando algo como 
- * GD, html ou outro formato de modo a permitir prever o funcionamento da classe
- * e mostar pelo browser
- * 
- */
