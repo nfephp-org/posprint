5f787c92e5d70d401a07e0b45c87ddf7c9a237ba
Roberto L. Machado <linux.rlm@gmail.com>
2016-06-02 09:17:59 -0300

    [UPD] Ajustes em Graphics

--- examples/convertImage2BMP.php
+++ examples/convertImage2BMP.php
@@ -5,10 +5,10 @@ use Posprint\Graphics\Graphics;
 
 $graph = new Graphics();
 
-$imgPath = 'tux.png';
+$imgPath = '../images/tux.png';
 
 $graph->load($imgPath);
 
 
-$imgPathBW = 'tux.bmp';
+$imgPathBW = '../images/tux.bmp';
 $graph->save($imgPathBW, 'BMP');
--- src/Graphics/Graphics.php
+++ src/Graphics/Graphics.php
@@ -117,17 +117,29 @@ class Graphics extends Basic
     
     /**
      * Converts a true color image to Black and white
+     * even if the image have transparency (alpha channel)
      */
     public function convertBW()
     {
         $newimg = imagecreatetruecolor($this->imgWidth, $this->imgHeight);
         imagealphablending($newimg, false);
         imagesavealpha($newimg, true);
-        imagecopyresampled($newimg, $this->img, 0, 0, 0, 0, $this->imgWidth, $this->imgHeight, $this->imgWidth, $this->imgHeight);
+        imagecopyresampled(
+            $newimg,
+            $this->img,
+            0,
+            0,
+            0,
+            0,
+            $this->imgWidth,
+            $this->imgHeight,
+            $this->imgWidth,
+            $this->imgHeight
+        );
         $bcg = imagecolorallocate($newimg, 255, 255, 255);
         imagefill($newimg, 0, 0, $bcg);
         imagefilter($newimg, IMG_FILTER_GRAYSCALE);
-        imagefilter($newimg, IMG_FILTER_CONTRAST, -255);
+        imagefilter($newimg, IMG_FILTER_CONTRAST, -1000);
         $this->img = $newimg;
     }
     
@@ -143,7 +155,7 @@ class Graphics extends Basic
     {
         $type = strtoupper($type);
         if ($type == 'BMP') {
-            $this->convert2BMP($filename);
+            $this->saveBMP($filename);
             return true;
         }
         $aTypes = ['PNG', 'JPG', 'JPEG',  'GIF'];
@@ -153,191 +165,6 @@ class Graphics extends Basic
         return $this->saveImage($filename, $this->img, $type, $quality);
     }
     
-    /**
-     * Convert a GD image into a BMP string representation
-     *
-     * @param string $filename
-     * @return string
-     */
-    public function convert2BMP($filename = null)
-    {
-        if (! is_resource($this->img)) {
-            return '';
-        }
-        //to remove alpha color and put white instead
-        $img = $this->img;
-        $imageX = imagesx($img);
-        $imageY = imagesy($img);
-        $bmp = '';
-        for ($yInd = ($imageY - 1); $yInd >= 0; $yInd--) {
-            $thisline = '';
-            for ($xInd = 0; $xInd < $imageX; $xInd++) {
-                $argb = self::getPixelColor($img, $xInd, $yInd);
-                $thisline .= chr($argb['blue']).chr($argb['green']).chr($argb['red']);
-            }
-            while (strlen($thisline) % 4) {
-                $thisline .= "\x00";
-            }
-            $bmp .= $thisline;
-        }
-        $bmpSize = strlen($bmp) + 14 + 40;
-        // bitMapHeader [14 bytes] - http://msdn.microsoft.com/library/en-us/gdi/bitmaps_62uq.asp
-        $bitMapHeader  = 'BM'; // WORD bfType;
-        $bitMapHeader .= self::littleEndian2String($bmpSize, 4); // DWORD bfSize;
-        $bitMapHeader .= self::littleEndian2String(0, 2); // WORD bfReserved1;
-        $bitMapHeader .= self::littleEndian2String(0, 2); // WORD bfReserved2;
-        $bitMapHeader .= self::littleEndian2String(54, 4); // DWORD bfOffBits;
-        // bitMapInfoHeader - [40 bytes] http://msdn.microsoft.com/library/en-us/gdi/bitmaps_1rw2.asp
-        $bitMapInfoHeader  = self::littleEndian2String(40, 4); // DWORD biSize;
-        $bitMapInfoHeader .= self::littleEndian2String($imageX, 4); // LONG biWidth;
-        $bitMapInfoHeader .= self::littleEndian2String($imageY, 4); // LONG biHeight;
-        $bitMapInfoHeader .= self::littleEndian2String(1, 2); // WORD biPlanes;
-        $bitMapInfoHeader .= self::littleEndian2String(24, 2); // WORD biBitCount;
-        $bitMapInfoHeader .= self::littleEndian2String(0, 4); // DWORD biCompression;
-        $bitMapInfoHeader .= self::littleEndian2String(0, 4); // DWORD biSizeImage;
-        $bitMapInfoHeader .= self::littleEndian2String(2835, 4); // LONG biXPelsPerMeter;
-        $bitMapInfoHeader .= self::littleEndian2String(2835, 4); // LONG biYPelsPerMeter;
-        $bitMapInfoHeader .= self::littleEndian2String(0, 4); // DWORD biClrUsed;
-        $bitMapInfoHeader .= self::littleEndian2String(0, 4); // DWORD biClrImportant;
-        $data = $bitMapHeader.$bitMapInfoHeader.$bmp;
-        $this->saveImage($filename, $data);
-        return $data;
-    }
-    
-    /**
-     * loadBMP
-     * Create a GD image from BMP file
-     *
-     * @param  string $filename
-     * @return boolean
-     */
-    public function loadBMP($filename)
-    {
-        //open file as binary
-        if (! $f1 = fopen($filename, "rb")) {
-            throw InvalidArgumentException('Can not open file.');
-        }
-        //get properties from image file
-        $file = unpack("vfile_type/Vfile_size/Vreserved/Vbitmap_offset", fread($f1, 14));
-        if ($file['file_type'] != 19778) {
-            throw InvalidArgumentException('This file is not a BMP image.');
-        }
-        //get properties form image
-        $bmp = unpack('Vheader_size/Vwidth/Vheight/vplanes/vbits_per_pixel'.
-           '/Vcompression/Vsize_bitmap/Vhoriz_resolution'.
-           '/Vvert_resolution/Vcolors_used/Vcolors_important', fread($f1, 40));
-        //check deep of colors
-        $bmp['colors'] = pow(2, $bmp['bits_per_pixel']);
-        if ($bmp['size_bitmap'] == 0) {
-            $bmp['size_bitmap'] = $file['file_size'] - $file['bitmap_offset'];
-        }
-        $bmp['bytes_per_pixel'] = $bmp['bits_per_pixel']/8;
-        $bmp['bytes_per_pixel2'] = ceil($bmp['bytes_per_pixel']);
-        $bmp['decal'] = ($bmp['width']*$bmp['bytes_per_pixel']/4);
-        $bmp['decal'] -= floor($bmp['width']*$bmp['bytes_per_pixel']/4);
-        $bmp['decal'] = 4-(4*$bmp['decal']);
-        if ($bmp['decal'] == 4) {
-            $bmp['decal'] = 0;
-        }
-        $palette = array();
-        if ($bmp['colors'] < 16777216) {
-            $palette = unpack('V'.$bmp['colors'], fread($f1, $bmp['colors']*4));
-        }
-        //read all data form image but not the header
-        $img = fread($f1, $bmp['size_bitmap']);
-        fclose($f1);
-        //create a true color GD resource
-        $vide = chr(0);
-        $res = imagecreatetruecolor($bmp['width'], $bmp['height']);
-        $p = 0;
-        $y = $bmp['height']-1;
-        //read all bytes form original file
-        while ($y >= 0) {
-            $x=0;
-            while ($x < $bmp['width']) {
-                //get byte color from BMP
-                $color = $this->getBMPColor($bmp['bits_per_pixel'], $img, $vide, $p, $palette);
-                if ($color === false) {
-                    throw RuntimeException('Fail during conversion from BMP number bit per pixel incorrect!');
-                }
-                imagesetpixel($res, $x, $y, $color[1]);
-                $x++;
-                $p += $bmp['bytes_per_pixel'];
-            }
-            $y--;
-            $p += $bmp['decal'];
-        }
-        $this->img = $res;
-        return true;
-    }
-    
-    /**
-     * Get byte color form BMP
-     *
-     * @param integer $bpp bytes_per_pixel
-     * @param string $img bytes read of file
-     * @param string $vide
-     * @param integer $p
-     * @param integer $palette
-     * @return integer|boolean
-     */
-    private function getBMPColor($bpp, $img, $vide, $p, $palette)
-    {
-        switch ($bpp) {
-            case 24:
-                return unpack("V", substr($img, $p, 3).$vide);
-                break;
-            case 16:
-                $color = unpack("v", substr($img, $p, 2));
-                $blue = ($color[1] & 0x001f) << 3;
-                $green = ($color[1] & 0x07e0) >> 3;
-                $red = ($color[1] & 0xf800) >> 8;
-                $color[1] = $red * 65536 + $green * 256 + $blue;
-                return $color;
-                break;
-            case 8:
-                $color = unpack("n", $vide.substr($img, $p, 1));
-                $color[1] = $palette[$color[1]+1];
-                return $color;
-                break;
-            case 4:
-                $color = unpack("n", $vide.substr($img, floor($p), 1));
-                if (($p*2)%2 == 0) {
-                    $color[1] = ($color[1] >> 4) ;
-                } else {
-                    $color[1] = ($color[1] & 0x0F);
-                }
-                $color[1] = $palette[$color[1]+1];
-                return $color;
-                break;
-            case 1:
-                $color = unpack("n", $vide.substr($img, floor($p), 1));
-                if (($p*8)%8 == 0) {
-                    $color[1] = $color[1]>>7;
-                } elseif (($p*8)%8 == 1) {
-                    $color[1] = ($color[1] & 0x40)>>6;
-                } elseif (($p*8)%8 == 2) {
-                    $color[1] = ($color[1] & 0x20)>>5;
-                } elseif (($p*8)%8 == 3) {
-                    $color[1] = ($color[1] & 0x10)>>4;
-                } elseif (($P*8)%8 == 4) {
-                    $color[1] = ($color[1] & 0x8)>>3;
-                } elseif (($p*8)%8 == 5) {
-                    $color[1] = ($color[1] & 0x4)>>2;
-                } elseif (($p*8)%8 == 6) {
-                    $color[1] = ($color[1] & 0x2)>>1;
-                } elseif (($p*8)%8 == 7) {
-                    $color[1] = ($color[1] & 0x1);
-                }
-                $color[1] = $palette[$color[1]+1];
-                return $color;
-                break;
-            default:
-                return false;
-        }
-    }
-
-
     /**
      * resizeImage
      * Resize an image
@@ -397,6 +224,124 @@ class Graphics extends Basic
             ->setLabelFontSize(8);
         $this->img = $qrCode->getImage();
         $this->getDimImage();
+    }
+    
+    /**
+     * loadBMP
+     * Create a GD image from BMP file
+     *
+     * @param  string $filename
+     * @return boolean
+     */
+    protected function loadBMP($filename)
+    {
+        //open file as binary
+        if (! $f1 = fopen($filename, "rb")) {
+            throw InvalidArgumentException('Can not open file.');
+        }
+        //get properties from image file
+        $file = unpack("vfile_type/Vfile_size/Vreserved/Vbitmap_offset", fread($f1, 14));
+        if ($file['file_type'] != 19778) {
+            throw InvalidArgumentException('This file is not a BMP image.');
+        }
+        //get properties form image
+        $bmp = unpack('Vheader_size/Vwidth/Vheight/vplanes/vbits_per_pixel'.
+           '/Vcompression/Vsize_bitmap/Vhoriz_resolution'.
+           '/Vvert_resolution/Vcolors_used/Vcolors_important', fread($f1, 40));
+        //check deep of colors
+        $bmp['colors'] = pow(2, $bmp['bits_per_pixel']);
+        if ($bmp['size_bitmap'] == 0) {
+            $bmp['size_bitmap'] = $file['file_size'] - $file['bitmap_offset'];
+        }
+        $bmp['bytes_per_pixel'] = $bmp['bits_per_pixel']/8;
+        $bmp['bytes_per_pixel2'] = ceil($bmp['bytes_per_pixel']);
+        $bmp['decal'] = ($bmp['width']*$bmp['bytes_per_pixel']/4);
+        $bmp['decal'] -= floor($bmp['width']*$bmp['bytes_per_pixel']/4);
+        $bmp['decal'] = 4-(4*$bmp['decal']);
+        if ($bmp['decal'] == 4) {
+            $bmp['decal'] = 0;
+        }
+        $palette = array();
+        if ($bmp['colors'] < 16777216) {
+            $palette = unpack('V'.$bmp['colors'], fread($f1, $bmp['colors']*4));
+        }
+        //read all data form image but not the header
+        $img = fread($f1, $bmp['size_bitmap']);
+        fclose($f1);
+        //create a true color GD resource
+        $vide = chr(0);
+        $res = imagecreatetruecolor($bmp['width'], $bmp['height']);
+        $p = 0;
+        $y = $bmp['height']-1;
+        //read all bytes form original file
+        while ($y >= 0) {
+            $x=0;
+            while ($x < $bmp['width']) {
+                //get byte color from BMP
+                $color = $this->getBMPColor($bmp['bits_per_pixel'], $img, $vide, $p, $palette);
+                if ($color === false) {
+                    throw RuntimeException('Fail during conversion from BMP number bit per pixel incorrect!');
+                }
+                imagesetpixel($res, $x, $y, $color[1]);
+                $x++;
+                $p += $bmp['bytes_per_pixel'];
+            }
+            $y--;
+            $p += $bmp['decal'];
+        }
+        $this->img = $res;
+        return true;
+    }
+
+    /**
+     * Convert a GD image into a BMP string representation
+     *
+     * @param string $filename
+     * @return string
+     */
+    protected function saveBMP($filename = null)
+    {
+        if (! is_resource($this->img)) {
+            return '';
+        }
+        //to remove alpha color and put white instead
+        $img = $this->img;
+        $imageX = imagesx($img);
+        $imageY = imagesy($img);
+        $bmp = '';
+        for ($yInd = ($imageY - 1); $yInd >= 0; $yInd--) {
+            $thisline = '';
+            for ($xInd = 0; $xInd < $imageX; $xInd++) {
+                $argb = self::getPixelColor($img, $xInd, $yInd);
+                $thisline .= chr($argb['blue']).chr($argb['green']).chr($argb['red']);
+            }
+            while (strlen($thisline) % 4) {
+                $thisline .= "\x00";
+            }
+            $bmp .= $thisline;
+        }
+        $bmpSize = strlen($bmp) + 14 + 40;
+        // bitMapHeader [14 bytes] - http://msdn.microsoft.com/library/en-us/gdi/bitmaps_62uq.asp
+        $bitMapHeader  = 'BM'; // WORD bfType;
+        $bitMapHeader .= self::littleEndian2String($bmpSize, 4); // DWORD bfSize;
+        $bitMapHeader .= self::littleEndian2String(0, 2); // WORD bfReserved1;
+        $bitMapHeader .= self::littleEndian2String(0, 2); // WORD bfReserved2;
+        $bitMapHeader .= self::littleEndian2String(54, 4); // DWORD bfOffBits;
+        // bitMapInfoHeader - [40 bytes] http://msdn.microsoft.com/library/en-us/gdi/bitmaps_1rw2.asp
+        $bitMapInfoHeader  = self::littleEndian2String(40, 4); // DWORD biSize;
+        $bitMapInfoHeader .= self::littleEndian2String($imageX, 4); // LONG biWidth;
+        $bitMapInfoHeader .= self::littleEndian2String($imageY, 4); // LONG biHeight;
+        $bitMapInfoHeader .= self::littleEndian2String(1, 2); // WORD biPlanes;
+        $bitMapInfoHeader .= self::littleEndian2String(24, 2); // WORD biBitCount;
+        $bitMapInfoHeader .= self::littleEndian2String(0, 4); // DWORD biCompression;
+        $bitMapInfoHeader .= self::littleEndian2String(0, 4); // DWORD biSizeImage;
+        $bitMapInfoHeader .= self::littleEndian2String(2835, 4); // LONG biXPelsPerMeter;
+        $bitMapInfoHeader .= self::littleEndian2String(2835, 4); // LONG biYPelsPerMeter;
+        $bitMapInfoHeader .= self::littleEndian2String(0, 4); // DWORD biClrUsed;
+        $bitMapInfoHeader .= self::littleEndian2String(0, 4); // DWORD biClrImportant;
+        $data = $bitMapHeader.$bitMapInfoHeader.$bmp;
+        $this->saveImage($filename, $data);
+        return $data;
     }
     
     /**
@@ -493,7 +438,7 @@ class Graphics extends Basic
      * @throws InvalidArgumentException
      * @throws RuntimeException
      */
-    public function saveImage($filename = null, $data = null, $type = 'PNG', $quality = 75)
+    protected function saveImage($filename = null, $data = null, $type = 'PNG', $quality = 75)
     {
         if (empty($filename) || empty($data)) {
             return false;
@@ -527,6 +472,72 @@ class Graphics extends Basic
             throw new RuntimeException("Fail to write in $filename.");
         }
         return true;
+    }
+
+    /**
+     * Get byte color form BMP
+     *
+     * @param integer $bpp bytes_per_pixel
+     * @param string $img bytes read of file
+     * @param string $vide
+     * @param integer $p
+     * @param integer $palette
+     * @return integer|boolean
+     */
+    private function getBMPColor($bpp, $img, $vide, $p, $palette)
+    {
+        switch ($bpp) {
+            case 24:
+                return unpack("V", substr($img, $p, 3).$vide);
+                break;
+            case 16:
+                $color = unpack("v", substr($img, $p, 2));
+                $blue = ($color[1] & 0x001f) << 3;
+                $green = ($color[1] & 0x07e0) >> 3;
+                $red = ($color[1] & 0xf800) >> 8;
+                $color[1] = $red * 65536 + $green * 256 + $blue;
+                return $color;
+                break;
+            case 8:
+                $color = unpack("n", $vide.substr($img, $p, 1));
+                $color[1] = $palette[$color[1]+1];
+                return $color;
+                break;
+            case 4:
+                $color = unpack("n", $vide.substr($img, floor($p), 1));
+                if (($p*2)%2 == 0) {
+                    $color[1] = ($color[1] >> 4) ;
+                } else {
+                    $color[1] = ($color[1] & 0x0F);
+                }
+                $color[1] = $palette[$color[1]+1];
+                return $color;
+                break;
+            case 1:
+                $color = unpack("n", $vide.substr($img, floor($p), 1));
+                if (($p*8)%8 == 0) {
+                    $color[1] = $color[1]>>7;
+                } elseif (($p*8)%8 == 1) {
+                    $color[1] = ($color[1] & 0x40)>>6;
+                } elseif (($p*8)%8 == 2) {
+                    $color[1] = ($color[1] & 0x20)>>5;
+                } elseif (($p*8)%8 == 3) {
+                    $color[1] = ($color[1] & 0x10)>>4;
+                } elseif (($P*8)%8 == 4) {
+                    $color[1] = ($color[1] & 0x8)>>3;
+                } elseif (($p*8)%8 == 5) {
+                    $color[1] = ($color[1] & 0x4)>>2;
+                } elseif (($p*8)%8 == 6) {
+                    $color[1] = ($color[1] & 0x2)>>1;
+                } elseif (($p*8)%8 == 7) {
+                    $color[1] = ($color[1] & 0x1);
+                }
+                $color[1] = $palette[$color[1]+1];
+                return $color;
+                break;
+            default:
+                return false;
+        }
     }
     
     /**
