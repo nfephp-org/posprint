521b80dc784ff06f7a033997922c7f09e1a4eb2e
Roberto L. Machado <linux.rlm@gmail.com>
2016-06-07 18:46:43 -0300

    [UPD] Colocado exemplo de Danfce para desenvolvimento

---
+++ examples/testDanfe.php
@@ -0,0 +1,17 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+include_once('../bootstrap.php');
+
+use Posprint\DanfcePos;
+use Posprint\Printers\Daruma;
+
+$printer = new Daruma();
+
+$danfe = new DanfcePos($printer);
+
+$filename = '../local/35160223050021000117650010000000091000000170-nfe.xml';
+
+$danfe->loadNFCe($filename);
+$danfe->monta();
+$danfe->printDanfe();
